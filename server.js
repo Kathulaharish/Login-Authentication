676d669a21790db643434e641dfe7c1b1992dc86
Hari <kathulaharish929311@gmail.com>
2023-10-09 13:21:36 +0530

    Database connection

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from 'express'
 import colors from 'colors'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
+import connectDB from './config/db.js'
 
 // this is an object
 const app = express()
 
 dotenv.config();
+
+//data base connection
+connectDB()
+
+//middleware
+app.use(express.json())
+app.use(morgan('dev'))
 
 //rest api
 app.get('/', (req, res)=>{
